887ba6a4b514c24b2364e964857d5a0fc368a5ed
WolnyJazdow <otwarty@jazdow.pl>
2026-03-28 02:09:43 +0100

    FAQ

---
+++ www/.vuepress/theme/components/oj-faq-item.vue
@@ -0,0 +1,35 @@
+<template lang='pug'>
+	.oj-faq-item
+		h3.question(@click="open = !open")
+			| {{question}}
+			span.toggle {{ open ? ' ↑' : ' ↓' }}
+		.answer(:class="{open, closed: !open}")
+			slot
+</template>
+
+<script>
+export default {
+	name: 'oj-faq-item',
+	props: { question: String },
+	data() { return { open: false } }
+}
+</script>
+
+<style scoped lang='stylus'>
+@import '../styles/component'
+.question
+	cursor pointer
+	user-select none
+	color $oj-violet
+	&:hover
+		opacity 0.8
+.toggle
+	font-weight 400
+	font-size 0.9em
+.answer
+	overflow hidden
+	&.open
+		animation expander-open .2s ease
+	&.closed
+		animation expander-close .2s ease forwards
+</style>
